35f158d0afbb616d9dd5c6783f4ea12a79022d26
HeT-Delwadiya <hetdelwadiya@gmail.com>
2023-08-25 23:04:37 +0530

    formated date of postedAt field

--- models/PostDTO.js
+++ models/PostDTO.js
@@ -6,7 +6,7 @@ class Post {
             title: post.news_obj.title,
             author: post.news_obj.author_name,
             content: post.news_obj.content,
-            postedAt: new Date(post.news_obj.created_at ?? 981495183000),
+            postedAt: new Date(post.news_obj.created_at ?? 981495183000).toLocaleString(),
             sourceURL: post.news_obj.source_url,
             readMore: post.news_obj.source_url
         }
